81aadea0545e1ec24829bc1321650d7cf604e9b3
yorozc <yorozcolop49@gmail.com>
2025-02-08 22:24:11 -0800

    added boiler plate code and book object

---
+++ library.js
@@ -0,0 +1,14 @@
+const myLibrary = [];
+
+function Book(title, author, pages, read){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = ()=>{
+        return this.title + " by " + this.author + " , " + 
+        this.pages + " pages " + " , " + this.read;
+    }
+
+}
+

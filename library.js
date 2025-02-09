fd731c4bd6c1e3bd98eb9e1f67109a709f173da8
yorozc <yorozcolop49@gmail.com>
2025-02-08 23:19:45 -0800

    created a function that adds book to mylibrary array

--- library.js
+++ library.js
@@ -11,7 +11,18 @@ function Book(title, author, pages, read){
     }
 }
 
-function addBookToLibrary(){
-    
+function addBookToLibrary(event){
+    const addBookForm = document.querySelector("#addBook")
+    event.preventDefault();
+    const formData = new FormData(addBookForm);
+    const title = formData.get("title");
+    const author = formData.get("author");
+    const pages = formData.get("pages");
+    const completed = formData.get("read");
+
+    const newBook = new Book(title, author, pages, completed);
+    myLibrary.push(newBook);
+    addBookForm.reset();//resets form after submitting
 }
 
+console.log(myLibrary);

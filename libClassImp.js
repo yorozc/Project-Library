6a0bd59aada4324f5a268072649b1dff9cc65095
yorozc <143294600+yorozc@users.noreply.github.com>
2025-05-09 21:24:46 -0700

    created class implementation of library and added basic book class

---
+++ libClassImp.js
@@ -0,0 +1,10 @@
+const Book = class{ //private
+    constructor(title, author, pages, read){
+        this.id = crypto.randomUUID();
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+
+    }
+}

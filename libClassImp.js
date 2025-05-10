c699cb6fcb7a34d83e87469dd5d93d49f0b3753e
yorozc <143294600+yorozc@users.noreply.github.com>
2025-05-10 10:31:59 -0700

    created displayBookShelf and toggleForm

--- libClassImp.js
+++ libClassImp.js
@@ -19,9 +19,9 @@ const formVariables = () =>{
 
 const myLibrary = (() => { //encapsulation of book list
     let instance;
-    const library = [];
 
     return class{
+        #library = [];
         constructor(){
             if (instance){
                 return instance;
@@ -29,14 +29,14 @@ const myLibrary = (() => { //encapsulation of book list
             instance = this;
         }
         addItem(item){
-            library.push(item);
+            this.#library.push(item);
         }
     
         getItems(){
-            return [...library];
+            return [...this.#library];
         }
-    }
-})
+    };
+})();
 
 const Book = class{ //private
     constructor(title, author, pages, read){
@@ -54,7 +54,8 @@ const Book = class{ //private
 
 const addBookToLibrary = (event) =>{
     event.preventDefault();
-    const formData = new FormData(addBookForm);
+    const formVars = formVariables();
+    const formData = new FormData(formVars.getAddBookForm());
     const title = formData.get("title");
     const author = formData.get("author");
     const pages = formData.get("pages");
@@ -69,5 +70,81 @@ const addBookToLibrary = (event) =>{
 }
 
 const displayBookShelf = () => {
+    const formVars = formVariables();
+    const library = new myLibrary();
+    const libraryList = library.getItems();
+    const shelf = formVars.getShelf();
+    shelf.innerHTML = "";
+    for (i=0; i < libraryList.length; i++){
+        //add delete button and something to show it completed
+        let bookCard = document.createElement("div");
+        let bookTitle = document.createElement("h1");
+        let bookAuthor = document.createElement("h4");
 
+        let bottomPart = document.createElement("div"); //contains bookStatus & removeBook
+        let bookStatus = document.createElement("img");
+        let removeBook = document.createElement("button")
+
+        bookTitle.textContent = myLibrary[i].title;
+        bookAuthor.textContent = myLibrary[i].author;
+
+        bookCard.setAttribute("class", "book");
+        bookCard.setAttribute("data-id", myLibrary[i].id);
+
+        bottomPart.setAttribute("class", "bottomOfCard")
+
+        // bottom left image of a check or x 
+        bookStatus.setAttribute("id", "status");
+        bookStatus.setAttribute("src", checkForStatus(myLibrary[i].read))
+
+        removeBook.setAttribute("class", "removeBook");
+        removeBook.textContent = "DEL";
+        removeBook.type = "button";
+        removeBook.style.display = "none";
+        
+        bottomPart.appendChild(bookStatus);
+        bottomPart.appendChild(removeBook)
+
+        bookCard.appendChild(bookTitle);
+        bookCard.appendChild(bookAuthor);
+        bookCard.appendChild(bottomPart);
+        bookCard.addEventListener("mouseover", () => {
+            removeBook.style.display = "flex";
+        });
+        bookCard.addEventListener("mouseout", () =>{
+            removeBook.style.display = "none";
+        })
+
+        removeBook.addEventListener("click", () => {
+            for (i=0; i < libraryList.length; i++){
+                if (bookCard.getAttribute("data-id") === libraryList[i].id){
+                    libraryList.splice(i, 1);
+                    bookCard.remove()
+                }
+            }
+            
+            console.log(library.getItems());
+        });
+
+        shelf.appendChild(bookCard);
+        bookForm.style.display = "none";
+    }
 }
+
+function toggleForm(){
+    const formVars = formVariables();
+    const bookForm = formVars.getBookForm();
+    open = false
+    formVars.getShowForm().addEventListener("click", ()=>{
+    if (open === false){
+        bookForm.style.display = "flex";
+        open = true
+    }else{
+        open = false
+        bookForm.style.display = "none";
+    }
+    })
+}
+
+toggleForm();
+displayBookShelf();
